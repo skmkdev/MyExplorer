779a1ee50629c466e8ac5941748cde7453c2b9b0
skmkdev <156962795+skmkdev@users.noreply.github.com>
2024-01-17 22:40:13 +0100

    Add files via upload

---
+++ MyExplorer/MyExplorer/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace MyExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelInfo = new System.Windows.Forms.Panel();
+            this.panelProperty = new System.Windows.Forms.Panel();
+            this.txtDriveInfo = new System.Windows.Forms.TextBox();
+            this.txtProperty = new System.Windows.Forms.TextBox();
+            this.panelDriver = new System.Windows.Forms.Panel();
+            this.listDrivers = new System.Windows.Forms.ListBox();
+            this.panelTree = new System.Windows.Forms.Panel();
+            this.listTypes = new System.Windows.Forms.CheckedListBox();
+            this.tree = new System.Windows.Forms.TreeView();
+            this.panelInfo.SuspendLayout();
+            this.panelProperty.SuspendLayout();
+            this.panelDriver.SuspendLayout();
+            this.panelTree.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelInfo
+            // 
+            this.panelInfo.Controls.Add(this.panelProperty);
+            this.panelInfo.Controls.Add(this.panelDriver);
+            this.panelInfo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelInfo.Location = new System.Drawing.Point(0, 0);
+            this.panelInfo.Name = "panelInfo";
+            this.panelInfo.Size = new System.Drawing.Size(278, 603);
+            this.panelInfo.TabIndex = 0;
+            // 
+            // panelProperty
+            // 
+            this.panelProperty.Controls.Add(this.txtDriveInfo);
+            this.panelProperty.Controls.Add(this.txtProperty);
+            this.panelProperty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelProperty.Location = new System.Drawing.Point(0, 219);
+            this.panelProperty.Name = "panelProperty";
+            this.panelProperty.Size = new System.Drawing.Size(278, 384);
+            this.panelProperty.TabIndex = 1;
+            // 
+            // txtDriveInfo
+            // 
+            this.txtDriveInfo.BackColor = System.Drawing.Color.White;
+            this.txtDriveInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDriveInfo.Location = new System.Drawing.Point(10, 3);
+            this.txtDriveInfo.Multiline = true;
+            this.txtDriveInfo.Name = "txtDriveInfo";
+            this.txtDriveInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDriveInfo.Size = new System.Drawing.Size(262, 135);
+            this.txtDriveInfo.TabIndex = 1;
+            // 
+            // txtProperty
+            // 
+            this.txtProperty.BackColor = System.Drawing.Color.White;
+            this.txtProperty.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProperty.Location = new System.Drawing.Point(10, 141);
+            this.txtProperty.Multiline = true;
+            this.txtProperty.Name = "txtProperty";
+            this.txtProperty.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtProperty.Size = new System.Drawing.Size(262, 231);
+            this.txtProperty.TabIndex = 0;
+            // 
+            // panelDriver
+            // 
+            this.panelDriver.Controls.Add(this.listDrivers);
+            this.panelDriver.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDriver.Location = new System.Drawing.Point(0, 0);
+            this.panelDriver.Name = "panelDriver";
+            this.panelDriver.Size = new System.Drawing.Size(278, 219);
+            this.panelDriver.TabIndex = 0;
+            // 
+            // listDrivers
+            // 
+            this.listDrivers.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listDrivers.FormattingEnabled = true;
+            this.listDrivers.ItemHeight = 24;
+            this.listDrivers.Location = new System.Drawing.Point(10, 20);
+            this.listDrivers.Margin = new System.Windows.Forms.Padding(10);
+            this.listDrivers.Name = "listDrivers";
+            this.listDrivers.Size = new System.Drawing.Size(262, 196);
+            this.listDrivers.TabIndex = 0;
+            this.listDrivers.SelectedIndexChanged += new System.EventHandler(this.listDrivers_SelectedIndexChanged);
+            // 
+            // panelTree
+            // 
+            this.panelTree.Controls.Add(this.listTypes);
+            this.panelTree.Controls.Add(this.tree);
+            this.panelTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTree.Location = new System.Drawing.Point(278, 0);
+            this.panelTree.Name = "panelTree";
+            this.panelTree.Size = new System.Drawing.Size(952, 603);
+            this.panelTree.TabIndex = 1;
+            // 
+            // listTypes
+            // 
+            this.listTypes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listTypes.FormattingEnabled = true;
+            this.listTypes.Location = new System.Drawing.Point(760, 20);
+            this.listTypes.Name = "listTypes";
+            this.listTypes.Size = new System.Drawing.Size(180, 571);
+            this.listTypes.TabIndex = 1;
+            this.listTypes.SelectedValueChanged += new System.EventHandler(this.listTypes_SelectedValueChanged);
+            // 
+            // tree
+            // 
+            this.tree.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tree.Location = new System.Drawing.Point(0, 20);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(754, 571);
+            this.tree.TabIndex = 0;
+            this.tree.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.CollapseTree);
+            this.tree.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.ExpandTree);
+            this.tree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tree_AfterSelect);
+            this.tree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tree_NodeMouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1230, 603);
+            this.Controls.Add(this.panelTree);
+            this.Controls.Add(this.panelInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelInfo.ResumeLayout(false);
+            this.panelProperty.ResumeLayout(false);
+            this.panelProperty.PerformLayout();
+            this.panelDriver.ResumeLayout(false);
+            this.panelTree.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelInfo;
+        private System.Windows.Forms.Panel panelProperty;
+        private System.Windows.Forms.Panel panelDriver;
+        private System.Windows.Forms.Panel panelTree;
+        private System.Windows.Forms.ListBox listDrivers;
+        private System.Windows.Forms.TreeView tree;
+        private System.Windows.Forms.TextBox txtDriveInfo;
+        private System.Windows.Forms.TextBox txtProperty;
+        private System.Windows.Forms.CheckedListBox listTypes;
+    }
+}
+
---
+++ MyExplorer/MyExplorer/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyExplorer
+{
+    public partial class Form1 : Form
+    {
+        string fullPath;        
+        public Form1()
+        {
+            InitializeComponent();        
+            
+            DetectDrivers();
+        }
+
+        private void DetectDrivers()
+        {
+            foreach(DriveInfo drvInfo in DriveInfo.GetDrives())
+            {
+                listDrivers.Items.Add(drvInfo);
+            }
+        }
+
+        private void listDrivers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Display_Drive_Attr(listDrivers.SelectedItem.ToString());
+            tree.Nodes.Clear();
+            DriveInfo d = (DriveInfo)listDrivers.SelectedItem;
+            TreeNode rootNode;
+            rootNode = new TreeNode(@d.Name);
+            rootNode.ImageIndex = 0;
+            rootNode.SelectedImageIndex = 0;
+            rootNode.Tag = "folder";
+            tree.Nodes.Add(rootNode);
+            
+            if (d.IsReady)
+            {
+                FillNodes(rootNode);
+            }
+            
+        }
+        
+        private void tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node == null) return;
+
+            if (File.Exists(tree.SelectedNode.FullPath))
+            {
+                txtProperty.Text = e.Node.FullPath;
+                Display_File_Attr(tree.SelectedNode.FullPath);
+            }
+            else
+            {
+                txtProperty.Text = e.Node.FullPath;
+                //close file related information                
+                Display_Dir_Attr(tree.SelectedNode.FullPath);
+            }
+
+        }
+        private void Display_Drive_Attr(string drvname)
+        {
+            DriveInfo drinfo = new DriveInfo(drvname);
+            string strInfo = "";
+
+            strInfo += "DriverName:=" + drinfo.Name.ToString() + Environment.NewLine;
+            strInfo += "Label:=" + drinfo.VolumeLabel.ToString() + Environment.NewLine;
+            strInfo += "FreeSpace:=" + drinfo.TotalFreeSpace.ToString() + Environment.NewLine;
+            strInfo += "TotalSize:=" + drinfo.TotalSize.ToString() + Environment.NewLine;
+
+            txtDriveInfo.Text = strInfo;
+        }
+
+        private void Display_Dir_Attr(string dirname)
+        {
+
+            string strInfo = "";
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(@dirname);
+                strInfo += "Name:=" + d.Name + Environment.NewLine;
+                strInfo += "NumberOfFiles:=" + d.GetFiles().Count() + Environment.NewLine;
+                strInfo += "Attributes:=" + File.GetAttributes(@dirname) + Environment.NewLine;
+                txtProperty.Text = strInfo;
+            }
+            catch { }
+        }
+
+        private void Display_File_Attr(string filename)
+        {
+            string strInfo = "";
+            
+            FileInfo f = new FileInfo(@filename);
+            strInfo += "Name:=" + f.Name + Environment.NewLine;
+            strInfo += "FileSize:=" + f.Length + Environment.NewLine;
+            strInfo += "Attributes:=" + File.GetAttributes(@filename) + Environment.NewLine;
+            txtProperty.Text = strInfo;
+        }
+
+        private void tree_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                // Look for a file extension.
+                //if (File.Exists(e.Node.FullPath))
+                    System.Diagnostics.Process.Start(@e.Node.FullPath);
+            }
+            // If the file is not found, handle the exception and inform the user.
+            catch (System.ComponentModel.Win32Exception)
+            {
+                MessageBox.Show("Error", "File not found or couldn't be opened.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (SystemException)
+            {
+                //if user accidently double clicks something that is not handled don't through exceptions
+            }
+        }
+
+        private void FillNodes(TreeNode dirNode)
+        {
+            DirectoryInfo dir = new DirectoryInfo(dirNode.FullPath);
+            try
+            {
+                foreach (DirectoryInfo dirItem in dir.GetDirectories())
+                {
+                    TreeNode newNode = new TreeNode(dirItem.Name);
+                    newNode.Tag = "folder";
+                    dirNode.Nodes.Add(newNode);
+                    newNode.Nodes.Add("*");
+                }
+
+                
+                List<string> uncheckedTypes= new List<string>();
+                if (listTypes.Items.Count > 0)
+                {
+                    foreach (var item in listTypes.Items)
+                    {
+                        if (!listTypes.CheckedItems.Contains(item))
+                            uncheckedTypes.Add(item.ToString());
+                    }
+                }
+
+                foreach (FileInfo fileItem in dir.GetFiles())
+                {
+                    TreeNode newNode = new TreeNode(fileItem.Name);
+                    if(uncheckedTypes.Count == 0 || !uncheckedTypes.Contains(fileItem.Extension.ToLower()))
+                        dirNode.Nodes.Add(newNode);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CollapseTree(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Parent != null)
+            {
+                e.Node.ImageIndex = 1;
+                e.Node.SelectedImageIndex = 1;
+                fullPath = e.Node.Parent.FullPath;
+                e.Node.Nodes.Clear();
+                e.Node.Nodes.Add("*");
+            }
+            else
+            {
+                fullPath = e.Node.FullPath;
+            }
+        }
+        TreeNode currentNode;
+        
+        private void ExpandTree(object sender, TreeViewEventArgs e)
+        {
+            currentNode = null;
+            if (e.Node.Nodes[0].Text == "*")
+            {
+                e.Node.ImageIndex = 2;
+                e.Node.SelectedImageIndex = 2;
+                fullPath = e.Node.FullPath;
+                e.Node.Nodes.Clear();
+                currentNode = e.Node;
+
+                listTypes.Items.Clear();
+
+                try {                     
+                    DirectoryInfo dir = new DirectoryInfo(currentNode.FullPath);
+                    foreach (FileInfo fileItem in dir.GetFiles())
+                    {
+                        string type = fileItem.Extension;
+                        type = type.ToLower();
+                        if (!listTypes.Items.Contains(type))
+                        {
+                            listTypes.Items.Add(type, true);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+                FillNodes(currentNode);
+            }
+            else
+            {
+                fullPath = e.Node.FullPath;
+            }
+        }
+        
+        private void listTypes_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (currentNode == null) return;
+            currentNode.Nodes.Clear();
+            FillNodes(currentNode);
+        }
+
+    }
+}
